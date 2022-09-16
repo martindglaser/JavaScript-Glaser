159599277c8387e4555ad6523575921271b40220
martindglaser <martindglaser@gmail.com>
2022-09-15 22:22:17 -0300

    Primera entrega del Proyecto Final

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
-function caclularCuotas(){
-	let total = document.getElementById('total').value;
-	let cuotas = document.getElementById('cuotas').value;
-	if(cuotas==''){
-		alert('Seleccione numero de cuotas');
+const Articulos = [
+	{index: 1, articulo: 'Manzanas', precioKilo: 200},
+	{index: 2, articulo: 'Peras', precioKilo: 150},
+	{index: 3, articulo: 'Cerezas', precioKilo: 400},
+]
+
+function menu(){
+	let menu = prompt('Menu \n 1. Ver articulos \n 2. Comprar \n 3. Buscar articulo');
+	if(menu == 1){
+		listarArticulos();
+	}else if(menu == 2){
+		comprarArticulo();
+	}else if(menu == 3){
+		filtrar();
+	}
+}
+
+menu();
+
+function multiplicar(a,b){
+	return a * b;
+}
+
+function comprarArticulo(){
+	let consulta = 'Que articulo queres comprar?\n';
+
+	for(let i = 0; i < Articulos.length; i++){
+		consulta += Articulos[i].index +'. '+Articulos[i].articulo+'\n';
+	}
+
+	let articuloAComprar = parseInt(prompt(consulta)) - 1;
+	if(articuloAComprar < 0 || articuloAComprar >= Articulos.length){
 		return;
 	}
-	let valorPorCuota = total/cuotas;
-	alert('El valor que debera pagar por cada cuota es de $'+valorPorCuota);
-	return;
+	let cantidad = parseInt(prompt('Cuantos kilos queres llevar?'));
+
+	alert('El total a pagar es $'+ multiplicar(cantidad,Articulos[ articuloAComprar ].precioKilo) );
+	menu();
 }
 
 
-function getTotal(){
-	let PrecioArticulos = document.getElementsByClassName('precioArticulos');
-	let total = 0;
-	for (let i = 0; i < PrecioArticulos.length; i++) {
-		total += parseFloat(PrecioArticulos[i].value);
+function filtrar(){
+	let filtro = prompt('Filtro');
+
+
+	const Resultado = Articulos.filter((el) => el.articulo.includes(filtro))
+
+	let mensaje = 'Resultado: \n';
+	for (let i = 0; i < Resultado.length; i++) {
+		mensaje += 'Articulo: '+Resultado[i].articulo + ' - Precio Kilo: $'+Resultado[i].precioKilo+'\n';
 	}
-	document.getElementById('total').value = total;
+	alert(mensaje);
+	menu();
 }
-getTotal();
+
+
+function listarArticulos(){
+	let respuesta = '';
+	for(let i = 0; i < Articulos.length; i++){
+		respuesta += 'Articulo: '+Articulos[i].articulo + ' - Precio Kilo: $'+Articulos[i].precioKilo+'\n';
+	}
+	alert(respuesta);
+	menu();
+}
