45130d12a93e84cf5c33c33c63bcbb947b0d4abe
martindglaser <martindglaser@gmail.com>
2022-09-22 23:14:40 -0300

    Carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,64 +1,84 @@
-const Articulos = [
-	{index: 1, articulo: 'Manzanas', precioKilo: 200},
-	{index: 2, articulo: 'Peras', precioKilo: 150},
-	{index: 3, articulo: 'Cerezas', precioKilo: 400},
-]
-
-function menu(){
-	let menu = prompt('Menu \n 1. Ver articulos \n 2. Comprar \n 3. Buscar articulo');
-	if(menu == 1){
-		listarArticulos();
-	}else if(menu == 2){
-		comprarArticulo();
-	}else if(menu == 3){
-		filtrar();
-	}
-}
-
-menu();
-
-function multiplicar(a,b){
-	return a * b;
-}
-
-function comprarArticulo(){
-	let consulta = 'Que articulo queres comprar?\n';
-
-	for(let i = 0; i < Articulos.length; i++){
-		consulta += Articulos[i].index +'. '+Articulos[i].articulo+'\n';
-	}
-
-	let articuloAComprar = parseInt(prompt(consulta)) - 1;
-	if(articuloAComprar < 0 || articuloAComprar >= Articulos.length){
-		return;
-	}
-	let cantidad = parseInt(prompt('Cuantos kilos queres llevar?'));
-
-	alert('El total a pagar es $'+ multiplicar(cantidad,Articulos[ articuloAComprar ].precioKilo) );
-	menu();
+if(typeof localStorage.getItem('CarritoDeCompras') == 'object'){
+	localStorage.setItem('CarritoDeCompras', JSON.stringify([]));
 }
 
 
-function filtrar(){
-	let filtro = prompt('Filtro');
 
-
-	const Resultado = Articulos.filter((el) => el.articulo.includes(filtro))
-
-	let mensaje = 'Resultado: \n';
-	for (let i = 0; i < Resultado.length; i++) {
-		mensaje += 'Articulo: '+Resultado[i].articulo + ' - Precio Kilo: $'+Resultado[i].precioKilo+'\n';
-	}
-	alert(mensaje);
-	menu();
+class ElementosStorage {
+  constructor(idProducto,nombre,precio) {
+    this.idProducto = idProducto;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
 }
 
 
-function listarArticulos(){
-	let respuesta = '';
-	for(let i = 0; i < Articulos.length; i++){
-		respuesta += 'Articulo: '+Articulos[i].articulo + ' - Precio Kilo: $'+Articulos[i].precioKilo+'\n';
-	}
-	alert(respuesta);
-	menu();
+
+let elementos = document.getElementsByClassName("agregarElemento");
+
+
+
+let agregarArticulo = function() {
+    let botonId = this.id;
+    let nombreAgregar = document.getElementById(`nombre-${botonId}`).value;
+    let precioAgregar = document.getElementById(`precio-${botonId}`).value;
+
+
+
+    let CarritoViejo = JSON.parse(localStorage.getItem('CarritoDeCompras'));
+
+
+    let objetoElemento = new ElementosStorage(botonId,nombreAgregar,precioAgregar);
+ 
+    console.log(objetoElemento);
+
+    CarritoViejo.push(objetoElemento)
+
+    let CarritoNuevo = CarritoViejo;
+    
+    console.log(CarritoNuevo);
+
+    let CarritoNuevoReady = JSON.stringify(CarritoNuevo);
+
+    localStorage.setItem('CarritoDeCompras', CarritoNuevoReady);
+
+    getArticulosEnCarrito();
+};
+
+
+
+for (let i = 0; i < elementos.length; i++) {
+    elementos[i].addEventListener('click', agregarArticulo);
 }
+
+
+
+function getArticulosEnCarrito(){
+	let carritoDiv = document.getElementById('carrito');
+    let Carrito = JSON.parse(localStorage.getItem('CarritoDeCompras'));
+
+    carritoDiv.innerHTML = '';
+    let totalCarrito = 0;
+    for(let i = 0; i < Carrito.length; i++){
+    	carritoDiv.innerHTML += `
+    	<div>
+    		<h3>${Carrito[i]['nombre']}</h3>
+    		<h4>$${Carrito[i]['precio']}</h4>
+    		<hr>
+    	</div>`;
+    	totalCarrito+=parseFloat(Carrito[i]['precio']);
+    }
+
+    if(Carrito.length>0){
+    	document.getElementById('totalCarrito').innerText = '$'+totalCarrito;
+    }
+
+}
+getArticulosEnCarrito();
+
+
+document.getElementById('vaciarCarrito').addEventListener('click', function(){
+	localStorage.setItem('CarritoDeCompras', JSON.stringify([]));
+	document.getElementById('totalCarrito').innerText = '';
+	getArticulosEnCarrito();
+});
